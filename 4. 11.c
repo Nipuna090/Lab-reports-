5d7f7b3824e32ed209bc834eac235494aa217822
Nipuna090 <nipun.tandukar009@gmail.com>
2025-08-12 21:19:39 +0545

    Add files via upload

---
+++ 4. 11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char str1[] = {'A', 'n', 'i', 's', 'h', '\0'};
+    char str2[] = "Gautam";
+    printf("String 1: %s\n", str1);
+    printf("String 2: %s\n", str2);
+
+    return 0;
+}
+
